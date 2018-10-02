cc327279080a9d4e881bcd43410af11f6b3c0806
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-02 17:45:33 +0530

    Add files via upload

---
+++ multiples.js
@@ -0,0 +1,7 @@
+//multiples of 5 using for loop
+console.log("using for loop");//display output to the console
+console.log("----------------");//display output to the console
+for(i=1;i<=50;i++){
+    if(i%5==0)
+    console.log(i);//display output to the console
+}
---
+++ multiples2.js
@@ -0,0 +1,8 @@
+console.log("Using while loop");//displays output to the console
+console.log("----------------");//displays output to the console
+var i=1;//initializing variables
+while(i<=50){
+    if(i%5==0)
+        console.log(i);//displays output to the console
+        i++;//incrementing the value of i
+}
